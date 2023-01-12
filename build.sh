0ce18129363f5d9266c78d9c7672d59a0b346367
Rophy Tsai <111145158+yltsaize@users.noreply.github.com>
2023-01-12 18:32:08 +0800

    .devcontainer and Dockerfile

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yarn install --frozen-lockfile
+
+# tsc outputs type definitions to dist-types/ in the repo root, which are then consumed by the build
+yarn tsc
+
+# Build the backend, which bundles it all up into the packages/backend/dist folder.
+yarn build:backend
